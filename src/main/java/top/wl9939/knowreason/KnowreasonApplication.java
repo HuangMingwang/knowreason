4b3df072129dc3a66cab924621d8e841861bd4d2
huangmingwang <1125385880@qq.com>
2021-02-13 11:15:02 +0800

    Initial commit

---
+++ src/main/java/top/wl9939/knowreason/KnowreasonApplication.java
@@ -0,0 +1,13 @@
+package top.wl9939.knowreason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowreasonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowreasonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/wl9939/knowreason/KnowreasonApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wl9939.knowreason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowreasonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
